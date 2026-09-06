6225a3d028089b003147249c578dfca70950fb77
Cristian Dominguez <c.isaacdominguez@gmail.com>
2026-09-05 22:50:18 -0700

    start analysis page

---
+++ app/article/[slug]/page.tsx
@@ -0,0 +1,16 @@
+export default function AnalysisPage() {
+  return (
+    <>
+      <section className="m-4 sm:my-6 sm:mx-12 md:my-10 xl:my-16 2xl:my-22 pb-8 sm:pb-12 md:pb-16 xl:pb-22 2xl:pb-28 lg:mx-18 xl:mx-24 2xl:mx-auto 2xl:max-w-325 flex flex-col gap-12 border-b border-clay-200">
+        <div className="flex flex-col gap-6 items-center">
+          <h1 className="text-6xl lg:text-7xl font-serif text-center">
+            Article Analysis Page
+          </h1>
+          <p className="text-lg font-sans text-center max-w-150">
+            Something something horse shoe.
+          </p>
+        </div>
+      </section>
+    </>
+  );
+}
