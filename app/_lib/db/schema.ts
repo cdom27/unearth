767d1eb0a1e922497b2482cdd25288d15858378d
Cristian Dominguez <c.isaacdominguez@gmail.com>
2026-09-05 22:06:41 -0700

    add separate status columns (temp while separate task table is created)

--- app/_lib/db/schema.ts
+++ app/_lib/db/schema.ts
@@ -11,11 +11,9 @@ import {
 } from "drizzle-orm/pg-core";
 import { sql } from "drizzle-orm";
 
-export const statusEnum = pgEnum("status", [
-  "summarized",
-  "analyzed",
-  "claims_extracted",
-  "claims_verified",
+export const analysisTaskStatusEnum = pgEnum("analysis_task_status", [
+  "pending",
+  "running",
   "completed",
 ]);
 
@@ -81,7 +79,21 @@ export const analyses = pgTable("analyses", {
   meta: jsonb("meta").notNull(),
   factualScore: doublePrecision("factual_score"),
   biasScore: doublePrecision("bias_score"),
-  status: statusEnum("status").notNull().default("summarized"),
+  summaryStatus: analysisTaskStatusEnum("summary_status")
+    .notNull()
+    .default("pending"),
+  rhetoricalAnalysisStatus: analysisTaskStatusEnum("rhetorical_analysis_status")
+    .notNull()
+    .default("pending"),
+  claimExtractionStatus: analysisTaskStatusEnum("claim_extraction_status")
+    .notNull()
+    .default("pending"),
+  claimVerificationStatus: analysisTaskStatusEnum("claim_verification_status")
+    .notNull()
+    .default("pending"),
+  factualScoreStatus: analysisTaskStatusEnum("factual_score_status")
+    .notNull()
+    .default("pending"),
   createdAt: timestamp("created_at", { withTimezone: true })
     .notNull()
     .defaultNow(),
